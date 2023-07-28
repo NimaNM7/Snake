138ddd204599c9135d4755003e88fb53ae83637e
NimaNM7 <moazzen24@gmail.com>
2023-07-29 03:05:19 +0330

    complete logic of the game but it has bugs

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let direction = 0;
+let madeApple;
 
+//main class
 class snakePart {
     static allSnakeParts = [];
     x;
@@ -34,14 +36,9 @@ class snakePart {
             else if (direction == 2) part.y = backPart.y - 20;
             else if (direction == 3) part.x = backPart.x + 20;
         } else {
-            console.log(backPart.getBackPart());
             part.x = 2 * backPart.x - backPart.getBackPart().x;
             part.y = 2 * backPart.y - backPart.getBackPart().y;
         }
-        console.log("current part:");
-        console.log(part);
-        console.log("back part:");
-        console.log(backPart);
     }
 
     static moveAll() {
@@ -51,8 +48,6 @@ class snakePart {
     }
 
     static headMove(part,dir) {
-        console.log('before moving');
-        console.log(part);
         let xChange = 0;
         let yChange = 0;
         if (dir == 0) yChange = -20;
@@ -60,12 +55,9 @@ class snakePart {
         else if (dir == 2) yChange = 20;
         else if (dir == 3) xChange = -20;
         part.div.remove();
-        console.log(part);
         part.x += xChange;
         part.y += yChange;
         snakePart.makeSnakePartsDiv(part);
-        console.log('after moving');
-        console.log(part);
     }
 
     static move(part,finalX,finalY) {
@@ -89,12 +81,90 @@ class snakePart {
     }
 }
 
+//methods
+function resetGame() {
+    const bodyChildren = document.querySelectorAll("body > *");
+    Array.from(bodyChildren).forEach((child)=>{ child.remove(); });
+    while (snakePart.allSnakeParts.length != 0) {
+        snakePart.allSnakeParts.pop();
+    }
+}
 
-let head = new snakePart();
-snakePart.allSnakeParts.push(head);
+function initGame() {
+    let headPart = new snakePart();
+    snakePart.allSnakeParts.push(headPart);
+    makeApple();
+}
 
-let secondPart = new snakePart();
-let thirdPart = new snakePart();
+
+function checkGame() {
+    snakePart.moveAll();
+    if (checkCollisionAll() || checkMargin())
+        loseGame();
+    else if (checkCollision(snakePart.allSnakeParts[0].div,madeApple))
+        eatApple();
+}
+
+function loseGame() {
+    resetGame();
+    initGame();
+}
+
+function makeApple() {
+    madeApple = document.createElement("div");
+    madeApple.classList.add("Apple");
+    madeApple.style.left = (Math.random() * window.innerWidth) + 'px';
+    madeApple.style.top = (Math.random() * window.innerHeight) + 'px';
+    document.body.appendChild(madeApple);
+}
+
+function eatApple() {
+    madeApple.remove();
+    makeApple();
+    let addingPart = new snakePart();
+}
+
+function checkCollision(div1, div2) {
+    let rect1 = div1.getBoundingClientRect();
+    let rect2 = div2.getBoundingClientRect();
+
+    // Check for overlap
+    if (
+      rect1.left < rect2.right &&
+      rect1.right > rect2.left &&
+      rect1.top < rect2.bottom &&
+      rect1.bottom > rect2.top
+    ) {
+        return true;
+    }
+    return false;
+  }
+  
+
+function checkCollisionAll() {
+    for (let i = 0 ; i < snakePart.allSnakeParts.length - 1 ; i++) {
+        for (let j = i+1 ; j < snakePart.allSnakeParts.length ; j++) {
+            if (checkCollision(snakePart.allSnakeParts[i].div,snakePart.allSnakeParts[j].div))
+                return true;
+        }
+    }
+    return false;
+}
+
+function checkMargin() {
+    let mainDiv = snakePart.allSnakeParts[0].div;
+    let div = mainDiv.getBoundingClientRect();
+    if (div.left < 0 || div.top < 0) return true;
+    if (div.right > window.innerWidth || div.bottom > window.innerHeight) return true;
+    return false;
+}
+
+
+
+//main 
+initGame();
+
+// for (let i = 0 ; i < 10 ; i++) new snakePart();
 
 document.addEventListener("keydown",(event) => {
     if(event.code == 'ArrowUp') direction = 0;
@@ -104,4 +174,4 @@ document.addEventListener("keydown",(event) => {
 })
 
 
-// setInterval(snakePart.moveAll,300);
+setInterval(checkGame,50);
